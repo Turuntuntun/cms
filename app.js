73b2b69a124f419bb8acce53da41f74485403bfc
SV-1 <vkserg@mail.com>
2020-06-30 00:39:55 +0300

    Test request

---
+++ app.js
@@ -0,0 +1,58 @@
+// Первый слайдер
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("store-main-capsule");
+  var dots = document.getElementsByClassName("carousel-thumbs__switch");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "flex";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
+// Второй слайдер акций
+var slideIndex = 1;
+showStocks(slideIndex);
+
+function plusStocks(n) {
+  showStocks(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showStocks(slideIndex = n);
+}
+
+function showStocks(n) {
+  var i;
+  var slides = document.getElementsByClassName("carousel-items-compilation");
+  var dots = document.getElementsByClassName("carousel-thumbs__swh");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "flex";  
+  dots[slideIndex-1].className += " active";
+}
