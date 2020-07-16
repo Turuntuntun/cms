257fd0c6ebb6d7d2ca1ab57d2d3b0554bc7e620f
u0985460 <u0985460@server68.hosting.reg.ru>
2020-07-16 15:04:52 +0300

    Izmenenie index.php

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 
 ini_set('max_execution_time', 9000);
 
+require_once('lib/dev.php');
 
-require_once($_SERVER['DOCUMENT_ROOT'].'/lib/dev.php');
-
-use lib\Parse;
+use lib\parse;
 
 spl_autoload_register(function ($class_name) {
     $path = str_replace('\\','/',$class_name.'.php');
+
     if (file_exists($path)){
         require_once $path;
     }
 });
-
 session_start();
+$parse = new parse('https://store.steampowered.com/?l=russian');
